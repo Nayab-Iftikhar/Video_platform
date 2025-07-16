c8cafe35d72251fc5268df7059810875a144902e
dev <dev@MacBook-Pro.local>
2025-07-16 16:16:48 +0500

    updated models

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,11 +2,10 @@ class Project < ApplicationRecord
   belongs_to :client, class_name: "User"
   belongs_to :project_manager, class_name: "User"
 
-  enum status: {
+  enum :status,
     pending: "pending",
     in_progress: "in_progress",
     completed: "completed"
-  }
 
   validates :name, :status, :footage_url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
-  enum role: { client: 0, project_manager: 1 }
+  enum :role, client: 0, project_manager: 1
 
-  validates :email, presence: true, uniqueness: true
+  validates :email_address, presence: true, uniqueness: true
   validates :name, presence: true
 end
