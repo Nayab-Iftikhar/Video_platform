1117baba694839b1cd6bd8732ed765829dd17498
dev <dev@MacBook-Pro.local>
2025-07-16 05:45:27 +0500

    Added user attributes roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  enum role: { client: 0, project_manager: 1 }
+
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
---
+++ db/migrate/20250716003712_add_details_to_users.rb
@@ -0,0 +1,6 @@
+class AddDetailsToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :role, :integer, null: false, default: 0
+  end
+end
